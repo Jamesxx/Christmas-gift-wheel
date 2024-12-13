7b9e2a61b649fbbe86fcbbf0c1023bfe45f44a84
Jamesxx <darocsk@hotmail.com>
2024-12-13 09:55:41 +0800

    Create page.js

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+'use client'
+import ChristmasGiftWheel from '../components/ChristmasGiftWheel'
+
+export default function Home() {
+  return (
+    <main>
+      <ChristmasGiftWheel />
+    </main>
+  )
+}
